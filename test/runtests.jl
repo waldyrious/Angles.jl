dd573b2a95dc6f0bd6a309e44ae0b239380980a3
Yakir Luc Gagnon <12.yakir@gmail.com>
2017-07-13 11:34:51 +0200

    improved coverage

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,9 @@ using Base.Test
 
 @test all(Radian(Degree(Radian(x))).x ≈ x for x in -10:rand():10)
 
+@test all(Degree(x) == x for x in Degree.(-10:rand():10))
+@test all(Radian(x) == x for x in Radian.(-10:rand():10))
+
 for fun in (:sin, :sinc, :cos, :cosc, :tan, :sec, :csc, :cot), degree in -89:50rand():89
     radian = deg2rad(degree)
     goal = @eval $fun($radian)
@@ -21,3 +24,4 @@ for fun in (:tan, :cot), x in -10:2rand():10
     @test @eval $(Symbol("arc$(fun)"))($x).x ≈ $(Symbol("a$(fun)"))($x)
 end
 
+@test all(arctan2(y, x) == Radian(atan2(y, x)) for x in -10:rand():10, y in -10:rand():10)
